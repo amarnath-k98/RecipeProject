374d95bb94aaffd915ac211075e12b430f384fed
amarnath-98 <amarmagesh717@gmail.com>
2025-10-04 14:02:13 +0530

    send email error corrected

--- Recipe-Server/src/utils/sendEmail.js
+++ Recipe-Server/src/utils/sendEmail.js
@@ -1,18 +1,25 @@
 import nodemailer from "nodemailer";
 
 export const sendEmail = async (to, subject, html) => {
-  const transporter = nodemailer.createTransport({
-    service: "Gmail",
-    auth: {
-      user: process.env.EMAIL_USER,
-      pass: process.env.EMAIL_PASS,
-    },
-  });
+  try {
+    const transporter = nodemailer.createTransport({
+      service: "Gmail",
+      auth: {
+        user: process.env.EMAIL_USER,
+        pass: process.env.EMAIL_PASS,
+      },
+    });
 
-  await transporter.sendMail({
-    from: `"Recipe App" <${process.env.EMAIL_USER}>`,
-    to,
-    subject,
-    html,
-  });
+    await transporter.sendMail({
+      from: `"Recipe App" <${process.env.EMAIL_USER}>`,
+      to,
+      subject,
+      html,
+    });
+
+    console.log("Email sent to:", to);
+  } catch (err) {
+    console.error("Email sending failed:", err);
+    throw err;
+  }
 };
